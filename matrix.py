e5be2737885e465f5bcc7a3da9231251e6eaf119
jwkim930 <jwkim930@naver.com>
2022-10-13 16:20:12 -0600

    Implemented enter()
    * seems to work as intended
    * '*' didn't get added after the first iteration
      for ienter(), so fixed that
    * might want to take out some common operations to simplify

--- matrix.py
+++ matrix.py
@@ -45,9 +45,8 @@ def ienter(row: int, col: int) -> np.ndarray:
       c += 1
       visual += " "
   visual = visual.rstrip()
-  
-  # take input and fill up
-  def replace(s:str, i:int, r:str) -> str:
+
+  def replace(s: str, i: int, r: str) -> str:
     """
     Returns the string with the character at given index replaced with r.
     :param s: string to have the replacement happen
@@ -56,8 +55,9 @@ def ienter(row: int, col: int) -> np.ndarray:
     :return: string after the replacement
     """
 
-    return s[0:i] + r + s[i+1:]
+    return s[0:i] + r + s[i + 1:]
 
+  # take input and fill up
   r = 0   # reset r for new iteration, c should've been reset from the first loop
   vis_i = 0   # this is the index of '*' in visual
   while r != row:
@@ -75,6 +75,7 @@ def ienter(row: int, col: int) -> np.ndarray:
       visual += " "
 
     vis_i += (len(new) + 1)   # new entry + 1 whitespace
+    visual = replace(visual, vis_i, "*")
 
   return mx
 
@@ -93,7 +94,117 @@ def enter(row:int, col:int, showto:int=2, sci:bool=False) -> np.ndarray:
   :return: a numpy array representing the entered matrix
   """
 
-  pass
+  assert type(row) is int and type(col) is int, "row/col must be integers"
+  assert row > 0 and col > 0, "row/col must be positive"
 
+  mx = np.zeros((row, col))
+  # build the visual representation
 
-print(ienter(3, 2))
+  # For sci == False, the matrix would look something like this (3 by 4 and showto == 2 for example):
+  # 0.00 0.00 0.00 0.00
+  # 1.00 1.00 **** ----
+  # ---- ---- ---- ----
+  if not sci:
+    keylen = 2 + showto   # number of */- to use, 2 for '0.'
+    visual = "*" * keylen
+    r = 0
+    c = 0
+    # insert whitespace appropriately
+    if c == col - 1:  # end of row reached
+      r += 1
+      c = 0
+      visual += "\n"
+    else:
+      c += 1
+      visual += " "
+
+    # add '-'s to the rest
+    while r != row:
+      visual += "-" * keylen
+      if c == col - 1:  # end of row reached
+        r += 1
+        c = 0
+        visual += "\n"
+      else:
+        c += 1
+        visual += " "
+
+  # For sci == True:
+  # 0.00E0 2.13E6 0.00E0 3.00E8
+  # 0.00E0 6.66E6 7.64E2 ******
+  # ------ ------ ------ ------
+  else:
+    keylen = 4 + showto  # 4 for '0.' and 'E0"
+    visual = "*" * keylen
+    r = 0
+    c = 0
+    # insert whitespace appropriately
+    if c == col - 1:  # end of row reached
+      r += 1
+      c = 0
+      visual += "\n"
+    else:
+      c += 1
+      visual += " "
+
+    # add '-'s to the rest
+    while r != row:
+      visual += "-" * keylen
+      if c == col - 1:  # end of row reached
+        r += 1
+        c = 0
+        visual += "\n"
+      else:
+        c += 1
+        visual += " "
+
+  visual = visual.rstrip()
+
+  def replace(s: str, i: tuple, r: str) -> str:
+    """
+    Returns the string with the characters at given index range replaced with r.
+    :param s: string to have the replacement happen
+    :param i: index range (inclusive lower bound, exclusive upper bound) of the characters to be replaced
+    :param r: string to be used for replacement
+    :return: string after the replacement
+    """
+
+    return s[0:i[0]] + r + s[i[1]:]
+
+  # take input and fill up
+  r = 0  # reset r for new iteration, c should've been reset from the first loop
+  vis_i = 0  # this is the index of the first '*' among '*'s in visual
+  while r != row:
+    print(visual)
+    if not sci:
+      new = input("Replace * with: ")
+      newf = float(new)
+    else:
+      new = input("Replace * with (in scientific notation, like 2.34E7): ").upper()
+      assert "E" in new, "invalid scientific notation"
+
+      newn = new.split("E")
+      newn[0] = float(newn[0])
+      newn[1] = int(newn[1])
+      newf = newn[0] * (10 ** newn[1])
+
+    mx[r, c] = newf
+
+    if not sci:
+      new = format(round(newf, showto), "." + str(showto) + "f")   # round to showto-th place
+    else:
+      new = format(round(newn[0], showto), "." + str(showto) + "f") + "E" + str(newn[1])
+
+    visual = replace(visual, (vis_i, vis_i + keylen), new)
+    if c == col - 1:  # end of row reached
+      r += 1
+      c = 0
+      visual += "\n"
+    else:
+      c += 1
+      visual += " "
+
+    vis_i += len(new) + 1   # new entry + 1 whitespace
+    visual = replace(visual, (vis_i, vis_i + keylen), "*" * keylen)
+
+  return mx

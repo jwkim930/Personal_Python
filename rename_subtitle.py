09a8c1f7af12ace4bbc986cc3fb3e81e7455cf42
jwkim930 <jwkim930@naver.com>
2023-03-16 15:30:39 -0600

    Mostly reimplemented rename_subtitle.py
    * The program does not rely on brackets to identify
      episode numbers, instead asking the user to verify
      what seems like the episode number.
    * The episode number do not need to match between
      the video and the subtitle as long as the number
      goes up consecutively.
    * Has a potential to allow ignoring the part after
      the episode number, accommodating for little
      difference in encoding information.

--- rename_subtitle.py
+++ rename_subtitle.py
@@ -1,8 +1,6 @@
 # The naming scheme of both the subtitle and video must be
 # consistent, differing only in the number.
-# Both the subtitle and video must be numbered the same way.
 # Every consecutive number must be present.
-# The first episode must be numbered 0 or 1 (or 00 or 01).
 # This script never modifies the video name.
 
 import os
@@ -14,69 +12,84 @@ vidName = input("Name of first video (including extension): ")
 assert vidName in files, "File with such name could not be found in local directory."
 
 
-def nameExtract(name:str) -> tuple[list[str], str]:
-    bPassed = False   # marks whether the index is past the first open bracket
-    ePassed = False   # marks whether the index is past the file extension
-    # iterate backward, the first 1 or 0 after open bracket is episode number
-    for i in range(len(name) - 1, -1, -1):
-        if ePassed:
-            if name[i] == '(' and i != 0:
-                # i == 0 implies RAW-name encased in open brackets, ignore this
-                bPassed = True
-            elif bPassed and (name[i] == '0' or name[i] == '1'):
-                if name[i - 1] == '0':
-                    # episode number is 00 or 01
-                    lst = [name[0:i - 1], name[i + 1:exti], ext]
-                    num = name[i - 1:i + 1]
-                else:
-                    # episode number is 0 or 1
-                    lst = [name[0:i], name[i + 1:exti], ext]
-                    num = name[i]
-                break
-        elif name[i] == '.':
+def nameExtract(name:str) -> list[str]:
+    """
+    Extract elements of name from the input filename.
+    The parts are as follows:
+    1. The characters before the episode count
+    2. The characters representing the episode number
+    3. The characters after the episode count, before file extension
+    4. File extension, including '.'
+
+    This function prompts the user to confirm the position of the episode number.
+
+    :param name: The file name to be split
+    :return: A list of 4 strings as described above
+    """
+    ext = ''   # file extension
+    for i in range(len(name)-1, -1, -1):
+        # separate file extension from name
+        if name[i] == '.':
             ext = name[i:]
-            exti = i
-            ePassed = True
-    if not bPassed:
-        # open bracket missing, ignore it
-        for i in range(len(name) - 1, -1, -1):
-            if name[i] == '0' or name[i] == '1':
-                if name[i - 1] == '0':
-                    # 00 or 01
-                    lst = [name[0:i - 1], name[i + 1:exti], ext]
-                    num = name[i - 1:i + 1]
-                else:
-                    # 0 or 1
-                    lst = [name[0:i], name[i + 1:exti], ext]
-                    num = name[i]
+            name = name[0:i]
+            break
+    epis = ''   # episode number
+    pre = ''   # characters before epis
+    post = ''   # characters after epis
+    i = 0   # index of beginning of episode name
+    j = 0   # index of end of episode name
+    while epis == '':
+        while i < len(name):
+            if name[i].isnumeric():
+                j = i
+                while name[j+1].isnumeric() and j+1 < len(name):
+                    j += 1
                 break
-    return lst, num
+            else:
+                i += 1
+        assert i < len(name), "No number could be found in the filename: " + name + ext
+        resp = ''
+        while resp != 'y' and resp != 'n':
+            print()
+            print(name[0:i] + '***' + name[i:j+1] + '***' + name[j+1:])
+            print("Are the characters marked like ***num*** the episode number?")
+            resp = input("Enter Y or N: ").lower()
+        if resp == 'y':
+            pre = name[0:i]
+            epis = name[i:j+1]
+            post = name[j+1:]
+        else:
+            i = j + 1
+    return [pre, epis, post, ext]
 
 
-subList, subNum = nameExtract(subName)
-vidList, vidNum = nameExtract(vidName)
-assert int(subNum) == int(vidNum), "Subtitle and video start with different numbers."
+def add1(num:str):
+    """
+    Adds 1 to the episode number while retaining its general format.
 
-while subList[0] + subNum + subList[1] + subList[2] in files:
-    old_subName = subList[0] + subNum + subList[1] + subList[2]
-    new_subName = vidList[0] + vidNum + vidList[1] + subList[2]
-    if vidList[0] + vidNum + vidList[1] + vidList[2] in files:
+    :param num: episode number
+    :return: episode number + 1
+    """
+    n = int(num) + 1
+    nxt = str(n)
+    if len(nxt) < len(num):
+        # missing leading zeros, such as going from 01 to 2
+        diff = len(num) - len(nxt)
+        nxt = diff * '0' + nxt
+    return nxt
+
+
+subElem = nameExtract(subName)
+vidElem = nameExtract(vidName)
+
+while subElem[0] + subElem[1] + subElem[2] + subElem[3] in files:
+    old_subName = subElem[0] + subElem[1] + subElem[2] + subElem[3]
+    new_subName = vidElem[0] + vidElem[1] + vidElem[2] + subElem[3]
+    if vidElem[0] + vidElem[1] + vidElem[2] + vidElem[3] in files:
         os.rename(old_subName, new_subName)
         print(old_subName)
         print("->", new_subName)
-
-    if len(vidNum) == 1 or int(vidNum) >= 9:
-        # 1, 09, etc., no 0 in front for next number
-        vidNum = str(int(vidNum) + 1)
-    else:
-        # 00, 01, etc., 0 in front for next number
-        vidNum = '0' + str(int(vidNum) + 1)
-
-    if len(subNum) == 1 or int(subNum) >= 9:
-        # 1, 09, etc., no 0 in front for next number
-        subNum = str(int(subNum) + 1)
-    else:
-        # 00, 01, etc., 0 in front for next number
-        subNum = '0' + str(int(subNum) + 1)
+    subElem[1] = add1(subElem[1])
+    vidElem[1] = add1(vidElem[1])
 
 print("The script ran successfully.")

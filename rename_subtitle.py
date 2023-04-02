029005639598d894e47810ef917a4adc3593a23f
jwkim930 <jwkim930@naver.com>
2023-04-02 16:38:49 -0600

    Updated rename_subtitle.py
    * Re-enabled the code commented out for testing.

--- rename_subtitle.py
+++ rename_subtitle.py
@@ -7,9 +7,9 @@ import os
 
 files = os.listdir()
 subName = input("Name of first subtitle (including extension): ")
-#assert subName in files, "File with such name could not be found in local directory."
+assert subName in files, "File with such name could not be found in local directory."
 vidName = input("Name of first video (including extension): ")
-#assert vidName in files, "File with such name could not be found in local directory."
+assert vidName in files, "File with such name could not be found in local directory."
 
 
 def nameExtract(name:str) -> list[str]:

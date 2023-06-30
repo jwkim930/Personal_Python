4e95311f616d7ab542f1f23a1a79021c89c3e3d4
jwkim930 <jwkim930@naver.com>
2023-06-30 17:45:29 -0600

    Updated testsubgen.py
    * It now creates a new directory called 'Test'
      and creates files inside it.

--- testsubgen.py
+++ testsubgen.py
@@ -1,4 +1,5 @@
 import random as rand
+import os
 
 VFORMATH = "[Legit] Video Name - "   # First part of the video name desired
 VFORMATT = " (1920x1080 AAC).mkv"   # Last part of the video name desired
@@ -27,11 +28,13 @@ def add1(num:str):
     return nxt
 
 
+if not os.path.isdir("Test"):
+    os.mkdir(os.getcwd() + "\\Test")
 for i in range(TNUM):
     sub_tail = " [" + str(rand.randint(100000, 999999)) + "]" + SFORMATT if RANDOMIZE_SUB else SFORMATT
     vid_tail = " [" + str(rand.randint(100000, 999999)) + "]" + VFORMATT if RANDOMIZE_VID else VFORMATT
-    vname = VFORMATH + VNUM + vid_tail
-    sname = SFORMATH + SNUM + sub_tail
+    vname = os.getcwd() + "\\Test\\" + VFORMATH + VNUM + vid_tail
+    sname = os.getcwd() + "\\Test\\" + SFORMATH + SNUM + sub_tail
     open(vname, 'x').close()
     open(sname, 'x').close()
     VNUM = add1(VNUM)
